d106f651729f2a7fa15ba5da59eb7b1845c45f51
Galina798 <galina7778@bk.ru>
2025-09-02 05:53:47 +0700

    изменение метки валюты при выборе из комбобокса, функция скрытого перевода USD в другие валюты

--- IAR.py
+++ IAR.py
@@ -1,12 +1,55 @@
+import requests
+import json
+import pprint
 from tkinter import *
 from tkinter import messagebox as mb
 from tkinter import ttk
 
 
 # Зашить неотображаемую функцию перевода USD в 9 любых базовых валют
+# изменение метки валюты при выборе из комбобокса
+def update_t_label(event):
+    t_code = t_combobox.get()
+    t_name = t_cur[t_code]
+    t_label.config(text = t_name)
 
+# функция курса валют по отношению к доллару
+# для возможности определения стоимости криптовалюты в других валютах
 
+def hidden_exchange_rate():
+    t_code = t_combobox.get()
 
+    if t_code:
+        try:
+            response = requests.get('https://open.er-api.com/v6/latest/USD')
+            response.raise_for_status()
+            data = response.json()
+            if t_code in data['hidden_rates']:
+                exchange_rate = data['hidden_rates'][t_code]
+                t_name = t_cur[t_code]
+                print("Курс обмена", f"Курс: {exchange_rate} {t_name} за 1 USD")
+            else:
+                mb.showerror("Ошибка!", f"Валюта {t_code} не найдена")
+
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка: {e}")
+    else:
+        mb.showwarning("Внимание", "Введите код валюты!")
+
+ # переменная для определения курса валют по отношению к доллару
+t_cur = {
+    'RUB': 'Российиский рубль',
+    'USD': 'Американский доллар',
+    'EUR': 'Евро',
+    'GBP': 'Британский фунт стерлингов',
+    'JPY': 'Японская йена',
+    'CNY': 'Китайский юань',
+    'KZT': 'Казахский тенге',
+    'UZS': 'Узбекский сум',
+    'CHF': 'Швейцарский франк',
+    'AED': 'Дирхам ОАЭ',
+    'CAD': 'Канадский доллар',
+}
 
 # Интерфейс
 root = Tk()
@@ -16,7 +59,7 @@ root.geometry("400x350")
 # выбор криптовалюты, курс которой требуется узнать
 ttk.Label(text = "Криптовалюта").pack(padx=10, pady=5)
 
-cr_combobox = ttk.Combobox(values = list(cur.keys()))
+cr_combobox = ttk.Combobox(values = list(cr_cur.keys()))
 cr_combobox.pack(padx=10, pady=5)
 cr_combobox.bind("<<ComboboxSelected>>", update_cr_label)
 
